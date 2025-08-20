99136b954644dc66701976ac8f9b9ed1faea308b
markgaughan75-maker <markgaughan75@gmail.com>
2025-08-20 12:47:43 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: { extend: {} },
-  plugins: [],
-}
+  plugins: []
+};
